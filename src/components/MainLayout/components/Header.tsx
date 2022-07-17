fc3d1d431c2b391e95a5a96dec7160eef799000b
Julian Rojas <35241159+JulianRojasBet@users.noreply.github.com>
2022-07-17 11:33:00 -0500

    [Task 2] Add Serverless config (#1)

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -46,7 +46,7 @@ export default function Header() {
     <AppBar position="relative">
       <Toolbar>
         <Typography variant="h6" className={classes.title}>
-          <Link className={classes.homeLink} to="/">My Store!</Link>
+          <Link className={classes.homeLink} to="/">Snorku Store</Link>
         </Typography>
 
         {auth && (
