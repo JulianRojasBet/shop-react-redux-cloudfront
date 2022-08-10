2ca9d5af0efaaa91ae5eb2c4ca81475c8f86fae6
Julian Rojas <35241159+JulianRojasBet@users.noreply.github.com>
2022-08-09 22:26:42 -0500

    [Task 5] Integrate import service (#4)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://g2wfa1y430.execute-api.us-east-1.amazonaws.com/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://mxfj7and46.execute-api.us-east-1.amazonaws.com/import',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://g2wfa1y430.execute-api.us-east-1.amazonaws.com',
 };
