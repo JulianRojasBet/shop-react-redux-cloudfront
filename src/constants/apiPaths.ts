307ab95bd2559bd391485b148ccde58a75c7059a
Julian Rojas <julian.rojas@partners.goodrx.com>
2022-08-16 21:17:17 -0500

    [Task 6] Change microservices API URLs

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://g2wfa1y430.execute-api.us-east-1.amazonaws.com/products',
+  product: 'https://e1y7ll83ll.execute-api.us-east-1.amazonaws.com/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://mxfj7and46.execute-api.us-east-1.amazonaws.com/import',
+  import: 'https://pcsap9cmbe.execute-api.us-east-1.amazonaws.com/import',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://g2wfa1y430.execute-api.us-east-1.amazonaws.com',
+  cart: 'https://e1y7ll83ll.execute-api.us-east-1.amazonaws.com',
 };
 
 export default API_PATHS;
