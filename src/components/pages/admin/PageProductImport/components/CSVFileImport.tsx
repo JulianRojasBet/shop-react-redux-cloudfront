f7e1443852a535bf262a4dbfc808c489c861710f
Julian Rojas <julian.rojas@partners.goodrx.com>
2022-08-26 13:22:46 -0500

    [Task 7] Integrate authorization service

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -30,10 +30,20 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
   };
 
   const uploadFile = async (e: any) => {
+      const token = localStorage.getItem('authorization_token');
+      let headers = {};
+
+      if (token)
+        headers = {
+        ...headers,
+        Authorization: `Basic ${token}`
+      }
+
       // Get the presigned URL
       const response = await axios({
         method: 'GET',
         url,
+        headers,
         params: {
           name: encodeURIComponent(file.name)
         }
